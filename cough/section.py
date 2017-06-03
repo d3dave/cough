673f2baa2d0ae3602f8939ce116c4d26c1aeec46
David Dorfman <d3dave@users.noreply.github.com>
2017-06-03 16:34:42 +0300

    Properly document section class

--- cough/section.py
+++ cough/section.py
@@ -37,16 +37,50 @@ class SectionFlags(enum.IntFlag):
 class Section:
     """
     Header struct:
-      0	8	Name	An 8-byte, null-padded UTF-8 encoded string. If the string is exactly 8 characters long, there is no terminating null. For longer names, this field contains a slash (/) that is followed by an ASCII representation of a decimal number that is an offset into the string table. Executable images do not use a string table and do not support section names longer than 8 characters. Long names in object files are truncated if they are emitted to an executable file.
-      8	4	VirtualSize	The total size of the section when loaded into memory. If this value is greater than SizeOfRawData, the section is zero-padded. This field is valid only for executable images and should be set to zero for object files.
-    12	4	VirtualAddress	For executable images, the address of the first byte of the section relative to the image base when the section is loaded into memory. For object files, this field is the address of the first byte before relocation is applied; for simplicity, compilers should set this to zero. Otherwise, it is an arbitrary value that is subtracted from offsets during relocation.
-    16	4	SizeOfRawData	The size of the section (for object files) or the size of the initialized data on disk (for image files). For executable images, this must be a multiple of FileAlignment from the optional header. If this is less than VirtualSize, the remainder of the section is zero-filled. Because the SizeOfRawData field is rounded but the VirtualSize field is not, it is possible for SizeOfRawData to be greater than VirtualSize as well. When a section contains only uninitialized data, this field should be zero.
-    20	4	PointerToRawData	The file pointer to the first page of the section within the COFF file. For executable images, this must be a multiple of FileAlignment from the optional header. For object files, the value should be aligned on a 4-byte boundary for best performance. When a section contains only uninitialized data, this field should be zero.
-    24	4	PointerToRelocations	The file pointer to the beginning of relocation entries for the section. This is set to zero for executable images or if there are no relocations.
-    28	4	PointerToLinenumbers	The file pointer to the beginning of line-number entries for the section. This is set to zero if there are no COFF line numbers. This value should be zero for an image because COFF debugging information is deprecated.
-    32	2	NumberOfRelocations	The number of relocation entries for the section. This is set to zero for executable images.
-    34	2	NumberOfLinenumbers	The number of line-number entries for the section. This value should be zero for an image because COFF debugging information is deprecated.
-    36	4	Characteristics	The flags that describe the characteristics of the section. For more information, see section 4.1, “Section Flags.”
+
+    Offset	Size	Field
+    =====================================
+       0	  8	    Name
+       8	  4	    VirtualSize
+      12	  4	    VirtualAddress
+      16	  4	    SizeOfRawData
+      20	  4	    PointerToRawData
+      24	  4	    PointerToRelocations
+      28	  4	    PointerToLinenumbers
+      32	  2	    NumberOfRelocations
+      34	  2	    NumberOfLinenumbers
+      36	  4	    Characteristics
+
+    Name:
+        An 8-byte, null-padded UTF-8 encoded string. If the string is exactly 8 characters long, there is no
+        terminating null. For longer names, this field contains a slash (/) that is followed by an ASCII
+        representation of a decimal number that is an offset into the string table. Long names in object files are
+        truncated if they are emitted to an executable file.
+    VirtualSize:
+        The total size of the section when loaded into memory. Should be set to zero.
+    VirtualAddress:
+        The address of the first byte of the section relative to the image base when the section is loaded into memory.
+        This field is the address of the first byte before relocation is applied; for simplicity, compilers should set
+        this to zero. Otherwise, it is an arbitrary value that is subtracted from offsets during relocation.
+    SizeOfRawData:
+        The size of the section. If this is less than VirtualSize, the remainder of the section is zero-filled. Because
+        the SizeOfRawData field is rounded but the VirtualSize field is not, it is possible for SizeOfRawData to be
+        greater than VirtualSize as well. When a section contains only uninitialized data, this field should be zero.
+    PointerToRawData:
+        The file pointer to the first page of the section within the COFF file. The value should be aligned on a 4-byte
+        boundary for best performance. When a section contains only uninitialized data, this field should be zero.
+    PointerToRelocations:
+        The file pointer to the beginning of relocation entries for the section. This is set to zero if there are no
+        relocations.
+    PointerToLinenumbers:
+        The file pointer to the beginning of line-number entries for the section. This is set to zero if there are no
+        COFF line numbers.
+    NumberOfRelocations:
+        The number of relocation entries for the section.
+    NumberOfLinenumbers:
+        The number of line-number entries for the section.
+    Characteristics:
+        The flags that describe the characteristics of the section.
     """
     header_struct = struct.Struct('<8sLLLLLLHHL')
 
