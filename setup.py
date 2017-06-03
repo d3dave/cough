5e3744ce4a7dd97637fb3b57f04098db37cded0e
David Dorfman <d3dave@users.noreply.github.com>
2017-06-03 15:37:42 +0300

    Add license and readme, update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
+import os
 from setuptools import setup
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(root, 'README.md')) as readme_file:
+    long_description = readme_file.read()
 
 setup(
     name='cough',
     version='0.1',
     description='Write COFF object files',
+    long_description=long_description,
     author='David D. Dorfman',
     author_email='d3dave@users.noreply.github.com',
+    url='https://github.com/d3dave/cough',
+    packages=['cough'],
+    license='MIT',
+    keywords='coff pe obj build development',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: Microsoft :: Windows'
+    ],
 )
